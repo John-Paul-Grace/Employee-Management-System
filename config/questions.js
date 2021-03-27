23dae55b13185a2c794ce4b54f8d2bbcea4ef22c
JP-Grace <gracejohnpaul200@gmail.com>
2021-03-27 17:18:59 -0400

    implemented department menu and view all departments

--- config/questions.js
+++ config/questions.js
@@ -4,18 +4,33 @@ const questions = {
         {
             type: 'list',
             message: 'What would you like to do?',
-            name: 'menuChoice',
+            name: 'choice',
             choices: ['Manage departments', 'Manage roles', 'Manage employees', 'Exit']
         }
     ],
-
-    // A confirmation question to warn users that other data may be deleted as well
-    confirmation: [
+    // Choices for managing departments
+    departmentMenu: [
         {
             type: 'list',
-            message: 'This will delete all related information as well. Are you sure you want to continue?',
+            message: 'Department options:',
             name: 'choice',
-            choices: ['Continue', 'Cancel']
+            choices: ['View all departments', 'Add department', 'Update department', 'Remove department', 'Back']
+        }
+    ],
+    // Choices for managing roles
+    roleMenu: [
+
+    ],
+    // Choices for managing employees
+    employeeMenu: [
+
+    ],
+    // A confirmation question to warn users that other data may be deleted as well
+    confirm: [
+        {
+            type: 'confirm',
+            message: 'This will delete all related information as well. Are you sure you want to continue?',
+            name: 'choice'
         }
     ]
 }
--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 // Imports all necessary files and libraries
 const inquirer = require('inquirer');
+const consoleTable = require('console.table');
 const orm = require('./config/orm');
 const questions = require('./config/questions');
 
+// Functions for department management
+// =========================================================================
+function addDepartment() {
+    console.log('Add department');
+    departmentMenu();
+}
+
+function updateDepartment() {
+    console.log('Update department');
+    departmentMenu();
+}
+
+function removeDepartment() {
+    console.log('Remove department');
+    departmentMenu();
+}
+// =========================================================================
+
 // Runs menu for department options
 function departmentMenu() {
-    console.log('Department');
-    mainMenu();
+    inquirer.prompt(questions.departmentMenu).then((res) => {
+        switch (res.choice) {
+            case "View all departments":
+                // Logs an array of all department names
+                orm.select('name', 'departments', (result) => {
+                    console.table(result);
+                    departmentMenu();
+                });
+                break;
+
+            case "Add department":
+                addDepartment();
+                break;
+
+            case "Update department":
+                updateDepartment();
+                break;
+
+            case "Remove department":
+                removeDepartment();
+                break;
+
+            case "Back":
+                mainMenu();
+                break;
+        }
+    });
 }
 
 // Runs menu for role options
@@ -24,7 +68,7 @@ function employeeMenu() {
 // Creates main menu with options to access other menus
 function mainMenu() {
     inquirer.prompt(questions.mainMenu).then((res) => {
-        switch (res.menuChoice) {
+        switch (res.choice) {
             case "Manage departments":
                 departmentMenu();
                 break;
